85d5f972d48ab1b33a6c4cf191b08483d6801053
aqeeluser404 <aqeelhanslo@gmail.com>
2024-02-26 13:06:30 +0200

    Adjusted vue.config.js to build files into server. also built application and ready for production

--- client/src/PostService.js
+++ client/src/PostService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/api/posts/';
+// const url = 'http://localhost:5000/api/posts/';
+const url = 'api/posts/';
 
 class PostService {
     // Get Post
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const path = require('path');
+
+// creating proxy for backend
+// building directory changed to server/public 
+module.exports = {
+
+  outputDir: path.resolve(__dirname, '../server/public'),
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000'
+      }
+    }
+  }
+};
